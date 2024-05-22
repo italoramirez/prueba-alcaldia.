3e77104b0c1fa556c3fe59190f46c0733d56c16b
Italo Ramirez P <italoramirezp@gmail.com>
2024-05-22 11:19:22 -0500

    Agrega parámetros en dto de giardado

--- employee-api/app/Http/DataTransferObjects/User/SaveData.php
+++ employee-api/app/Http/DataTransferObjects/User/SaveData.php
@@ -10,6 +10,9 @@ class SaveData extends Data
     public function __construct(
         public string $email,
         public string $name,
+        public string $last_name,
+        public string $address,
+        public string $phone,
         public string $password
     )
     {
@@ -23,6 +26,9 @@ class SaveData extends Data
         return [
             'email' => 'required|max:255',
             'name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'address' => 'required|max:255',
+            'phone' => 'required|max:30',
             'password' => 'required|min:3|max:15'
         ];
     }
