d22a889281fd8d8dd9395c65563ab909f6ae91f8
Italo Ramirez P <italoramirezp@gmail.com>
2024-05-22 11:19:01 -0500

    Agrega método para registrar, logout

--- employee-api/app/Http/Controllers/Auth/AuthController.php
+++ employee-api/app/Http/Controllers/Auth/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\DataTransferObjects\Auth\LoginData;
+use App\Http\DataTransferObjects\User\SaveData;
 use App\Service\Auth\AuthService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -14,8 +16,31 @@ class AuthController extends Controller
     )
     {
     }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function register(Request $request): array
+    {
+        return $this->authService->register(SaveData::from($request));
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
     public function login(Request $request): array
     {
         return $this->authService->login(LoginData::from($request));
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function logout(): JsonResponse
+    {
+        $this->authService->removeAuthenticationToken(request()->user());
+        return response()->json([null => null], 204);
     }
 }
