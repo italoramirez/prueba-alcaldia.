db816ed182fe81d54edb617ec03cca466e3d7537
Italo Ramirez P <italoramirezp@gmail.com>
2024-05-22 11:20:02 -0500

    Registra en provider

--- employee-api/app/Providers/AppServiceProvider.php
+++ employee-api/app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Http\Repositories\User\Impl\UserRepositoryImpl;
+use App\Http\Repositories\User\UserRepository;
+use App\Service\Auth\AuthService;
+use App\Service\Auth\Impl\AuthServiceImpl;
+use App\Service\User\Impl\UserServiceImpl;
+use App\Service\User\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        //Services
+        $this->app->bind(AuthService::class, AuthServiceImpl::class);
+        $this->app->bind(UserService::class, UserServiceImpl::class);
+
+        //Repositories
+        $this->app->bind(UserRepository::class, UserRepositoryImpl::class);
     }
 
     /**
